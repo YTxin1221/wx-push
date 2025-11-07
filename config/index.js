6e7bacb6b2ebc83f905e1829acf54821a686a135
YTxin1221 <135513737+YTxin1221@users.noreply.github.com>
2025-11-07 17:42:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wxe5141640127f071b",
   appsecret: "984857d9596163be7d48b1d2fce6ee30",
-  template_id: "kmOURa0Lu0n18qDr8qNdgyeq-zbv9bz4VHBC-MZgbPI",
+  template_id: "GNP2LNGbzRRXLkzljkV6-wfMsZAqkqLH4UKuzWWl2LA",
   touser: "oqar42LZ_-j1s87lUu3syIeZlIV4",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
